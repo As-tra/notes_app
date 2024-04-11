382edfab4c71cbe7b841daaac927561313efc01c
Assil <astradhdbjd@gmail.com>
2024-04-11 20:40:45 +0100

    create notes list view

---
+++ lib/views/widgets/notes_list_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:note_offline/views/widgets/custom_note_item.dart';
+
+class NotesListView extends StatelessWidget {
+  const NotesListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return NoteItem(
+          title: 'Flutter tips',
+          text: 'Build your Career with Tharwat Samy',
+          date: 'May 21,2022',
+          color: Colors.orange.shade200,
+        );
+      },
+    );
+  }
+}
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,40 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:note_offline/views/widgets/custom_app_bar.dart';
-import 'package:note_offline/views/widgets/custom_note_item.dart';
+import 'package:note_offline/views/widgets/notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
+    return const Padding(
+      padding: EdgeInsets.symmetric(
         horizontal: 24,
         vertical: 16,
       ),
-      child: ListView(
+      child: Column(
         children: [
-          const CustomAppBar(),
-          NoteItem(
-            title: 'Flutter tips',
-            text: 'Build your Career with Tharwat Samy',
-            date: 'May 21,2022',
-            color: Colors.deepPurple.shade200,
-          ),
-          NoteItem(
-            title: 'Flutter tips',
-            text: 'Build your Career with Tharwat Samy',
-            date: 'May 21,2022',
-            color: Colors.green.shade200,
-          ),
-          NoteItem(
-            title: 'Flutter tips',
-            text: 'Build your Career with Tharwat Samy',
-            date: 'May 21,2022',
-            color: Colors.orange.shade200,
-          ),
+          CustomAppBar(),
+          Expanded(child: NotesListView()),
         ],
       ),
     );
   }
 }
+
+
