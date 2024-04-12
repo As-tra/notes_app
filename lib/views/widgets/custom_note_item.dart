29f9b2e3425dfe2486870d38e797443977f2c838
Assil <astradhdbjd@gmail.com>
2024-04-12 23:57:42 +0100

    create note edit view and add their navigation

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:note_offline/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   final String title;
@@ -16,55 +17,65 @@ class NoteItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 12),
-      padding: const EdgeInsets.all(24),
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        children: [
-          ListTile(
-            contentPadding: const EdgeInsets.all(0),
-            title: Padding(
-              padding: const EdgeInsets.only(bottom: 8.0),
-              child: Text(
-                title,
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 26,
-                ),
-              ),
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const FaIcon(
-                FontAwesomeIcons.trash,
-                size: 26,
-                color: Colors.black,
-              ),
-            ),
-            subtitle: Text(
-              text,
-              style: const TextStyle(
-                color: Colors.black45,
-                fontSize: 18,
-              ),
-            ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => const EditNoteView(),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Text(
-                date,
-                style: const TextStyle(
-                  color: Colors.black38,
+        );
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 12),
+        padding: const EdgeInsets.all(24),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          children: [
+            ListTile(
+              contentPadding: const EdgeInsets.all(0),
+              title: Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                  title,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 26,
+                  ),
                 ),
               ),
-            ],
-          )
-        ],
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const FaIcon(
+                  FontAwesomeIcons.trash,
+                  size: 26,
+                  color: Colors.black,
+                ),
+              ),
+              subtitle: Text(
+                text,
+                style: const TextStyle(
+                  color: Colors.black45,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  date,
+                  style: const TextStyle(
+                    color: Colors.black38,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
