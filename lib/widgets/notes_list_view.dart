a45398e4374353fa29ace8fef78a5661a43bc27b
Assil <astradhdbjd@gmail.com>
2024-04-18 21:36:43 +0100

    modify the note view body to handle the notes list states (empy or filled)

--- lib/widgets/notes_list_view.dart
+++ lib/widgets/notes_list_view.dart
@@ -1,26 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:note_offline/cubits/notesCubit/notes_cubit.dart';
-import 'package:note_offline/models/note_model.dart';
 import 'package:note_offline/widgets/custom_note_item.dart';
 
 class NotesListView extends StatelessWidget {
-  const NotesListView({super.key});
+  final List notes;
+  const NotesListView({
+    super.key,
+    required this.notes,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<NotesCubit, NotesState>(
-      builder: (context, state) {
-        BlocProvider.of<NotesCubit>(context).fetchAllNotes();
-        List<NoteModal> notes = BlocProvider.of<NotesCubit>(context).notes;
-        return ListView.builder(
-          padding: EdgeInsets.zero,
-          itemCount: notes.length,
-          itemBuilder: (context, index) {
-            return NoteItem(
-              note: notes[index],
-            );
-          },
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      itemCount: notes.length,
+      itemBuilder: (context, index) {
+        return NoteItem(
+          note: notes[index],
         );
       },
     );
--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note_offline/cubits/notesCubit/notes_cubit.dart';
+import 'package:note_offline/models/note_model.dart';
 import 'package:note_offline/widgets/custom_app_bar.dart';
 import 'package:note_offline/widgets/empty_list_view.dart';
 import 'package:note_offline/widgets/notes_list_view.dart';
@@ -14,6 +15,7 @@ class NotesViewBody extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.symmetric(
           horizontal: 24,
+          vertical: 8,
         ),
         child: Column(
           children: [
@@ -24,10 +26,17 @@ class NotesViewBody extends StatelessWidget {
             const SizedBox(
               height: 20,
             ),
-            Expanded(
-              child: BlocProvider.of<NotesCubit>(context).notes.isEmpty
-                  ? const EmptyListView()
-                  : const NotesListView(),
+            BlocBuilder<NotesCubit, NotesState>(
+              builder: (context, state) {
+                BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+                List<NoteModal> notes =
+                    BlocProvider.of<NotesCubit>(context).notes;
+                return Expanded(
+                  child: notes.isEmpty
+                      ? const EmptyListView()
+                      : NotesListView(notes: notes),
+                );
+              },
             ),
           ],
         ),
