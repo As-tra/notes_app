6e1d2db838799c375d271b65b8b55b1519aca31d
Assil <astradhdbjd@gmail.com>
2024-04-20 16:34:06 +0100

    add safeArea to the edit note view

--- lib/widgets/edit_view_body.dart
+++ lib/widgets/edit_view_body.dart
@@ -21,49 +21,51 @@ class _EditNoteViewBodyState extends State<EditNoteViewBody> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 24,
-        vertical: 16,
-      ),
-      child: Column(
-        children: [
-          CustomAppBar(
-            title: 'Edit Note',
-            icon: Icons.check,
-            onPressed: () {
-              // logic here to modify the note
-              widget.note.title = title ?? widget.note.title;
-              widget.note.content = content ?? widget.note.content;
-              widget.note.save();
-              // triger the notesCubit to show updates
-              BlocProvider.of<NotesCubit>(context).updateNotes();
-              // close the edit view
-              Navigator.of(context).pop();
-            },
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          CustomTextField(
-            onchanged: (value) {
-              title = value;
-            },
-            hintText: 'Title',
-            initialValue: widget.note.title,
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          CustomTextField(
-            onchanged: (value) {
-              content = value;
-            },
-            hintText: 'content',
-            numOfLines: 5,
-            initialValue: widget.note.content,
-          ),
-        ],
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+          vertical: 8,
+        ),
+        child: Column(
+          children: [
+            CustomAppBar(
+              title: 'Edit Note',
+              icon: Icons.check,
+              onPressed: () {
+                // logic here to modify the note
+                widget.note.title = title ?? widget.note.title;
+                widget.note.content = content ?? widget.note.content;
+                widget.note.save();
+                // triger the notesCubit to show updates
+                BlocProvider.of<NotesCubit>(context).updateNotes();
+                // close the edit view
+                Navigator.of(context).pop();
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            CustomTextField(
+              onchanged: (value) {
+                title = value;
+              },
+              hintText: 'Title',
+              initialValue: widget.note.title,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              onchanged: (value) {
+                content = value;
+              },
+              hintText: 'content',
+              numOfLines: 5,
+              initialValue: widget.note.content,
+            ),
+          ],
+        ),
       ),
     );
   }
